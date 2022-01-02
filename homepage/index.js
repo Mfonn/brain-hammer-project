caadf76c208eb58d24bcedeb8cbd24c9312db8c1
Mfon <mfonnta05@gmail.com>
2022-01-02 15:19:59 +0100

    first commit

---
+++ homepage/index.js
@@ -0,0 +1,9 @@
+// function openDialog() {
+//     var bgmodal = document.getElementById("myPopup");
+//     bgmodal.classList.toggle("show");
+// }
+
+function popUp() {
+    var bgmodal = document.getElementById("myPopup");
+    bgmodal.classList.toggle("show");
+  }
