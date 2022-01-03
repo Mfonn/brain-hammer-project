482841fb3fac77b5039b575ae0f320f532c0364f
Mfon <mfonnta05@gmail.com>
2022-01-03 14:52:00 +0100

    responsive homepage

---
+++ home/home.js
@@ -0,0 +1,23 @@
+window.onload = function() {
+  var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+}
+
+
