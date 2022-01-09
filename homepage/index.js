6497dc7e5ad6ac7a6f65a5bd41ea158a56020637
Mfon <mfonnta05@gmail.com>
2022-01-09 15:29:05 +0100

    Weekend changes

--- homepage/index.js
+++
@@ -1,9 +0,0 @@
-// function openDialog() {
-//     var bgmodal = document.getElementById("myPopup");
-//     bgmodal.classList.toggle("show");
-// }
-
-function popUp() {
-    var bgmodal = document.getElementById("myPopup");
-    bgmodal.classList.toggle("show");
-  }
---
+++ js/homepage.js
@@ -0,0 +1,58 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+
+  // for the signup modal 
+  window.onload = function(){ 
+    // Get the modal
+    var modall = document.getElementById("myModal2");
+    
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+    
+    
+    // When the user clicks on the button, open the modal
+    btn.onclick = function() {
+      modall.style.display = "block";
+    }
+    
+    
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modall) {
+        modall.style.display = "none";
+      } else if (event.target == modal) {
+          modal.style.display = "none"
+      }
+    }
+
+    // Get the modal
+    var modal = document.getElementById("myModal");
+    
+    // Get the button that opens the modal
+    
+    var btnn = document.getElementById("myBtnn");
+
+    // When the user clicks on the button, open the modal
+    
+    btnn.onclick = function() {
+      modal.style.display = "block";
+    }
+
+
+    }
+
+
+
+
+    
+
+
