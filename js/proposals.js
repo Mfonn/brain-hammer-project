df2a44db8d9cf4997ce6b1949abe2da5cb515e7f
Mfon <mfonnta05@gmail.com>
2022-01-21 01:25:24 +0100

    Thursday changes

--- js/proposals.js
+++ js/proposals.js
@@ -4,4 +4,26 @@ function openForm() {
   
   function closeForm() {
     document.getElementById("myForm").style.display = "none";
+  }    
+
+
+
+  // for the popup modify price box 
+
+  function myOption() {
+    document.getElementById("myModify").style.display = "block";
   }
+
+
+  // for the popup payment box 
+
+  function myPayment() {
+    document.getElementById("myPayment").style.display = "block";
+  } 
+  
+  function myClose() {
+    document.getElementById("myPayment").style.display = "none";
+  }    
+
+
+
